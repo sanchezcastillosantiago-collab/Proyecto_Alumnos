e1dc331a735d385144a89c19ad72ffc9f7088ac6
Santiago Sánchez Castillo <sanchezcastillosantiago@gmail.com>
2025-10-02 16:44:41 -0600

    Corregi los enlaces y la tablabla o tr y td en el index

--- resources/views/Alumnos/index-alumnos.blade.php
+++ resources/views/Alumnos/index-alumnos.blade.php
@@ -7,23 +7,54 @@
 </head>
 <body>
     <h1>Alumnos</h1>
-    <a href="{{ route('alumnos.create') }}">Crear Alumno</a>
+    
     <ul>
-        @foreach ($alumnos as $alumno)
-            <tr>
-                <td>{{ $alumno->nombre }}</td>
-                <td>{{ $alumno->correo }}</td>
-                <td>{{ $alumno->codigo }}</td>
-                <td>{{ $alumno->fecha_nacimiento }}</td>
-                <td>{{ $alumno->sexo }}</td>
-                <td>{{ $alumno->carrera }}</td>
-            </tr>
-        @endforeach
+        <li>
+            <a href="{{ route('alumnos.create') }}">Crear Alumno</a>
+        </li>
     </ul>
-    
-    <tr>
+    <table border="1" > 
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Correo</th>
+                <th>Código</th>
+                <th>Fecha de Nacimiento</th>
+                <th>Sexo</th>
+                <th>Carrera</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($alumnos as $alumno)
+        <tr>
+            <td>
+                {{ $alumnoalumno->id  }}
+            </td>
+            <td>
+                <a href="{{ route('alumnos.show', $alumno->id) }}">{{ $alumno->nombre }}</a>
+            </td>
+            <td>
+                {{ $alumno->correo }}
+            </td>
+            <td>
+                {{ $alumno->codigo }}
+            </td>
+            <td>
+                {{ $alumno->fecha_nacimiento }}
+            </td>
+            <td>
+                {{ $alumno->sexo }}
+            </td>
+            <td>
+                {{ $alumno->carrera }}
+            </td>
+        </tr>
 
-    </tr>
+    <td>
+        <a href ="{{ route('alumnos.editar', $alumno->id) }}">editar</a>
+    </td>
+        @endforeach
     
+        </tbody>
 </body>
 </html>
