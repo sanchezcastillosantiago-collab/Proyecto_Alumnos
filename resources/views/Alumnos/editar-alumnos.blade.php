7902da7f69a91c69d20508425d3c2519405c054a
Santiago Sánchez Castillo <sanchezcastillosantiago@gmail.com>
2025-10-15 19:08:41 -0600

    Se crearon slots y se agregaron pruebas de layout pero se regrso a usar Bootstrap

--- resources/views/Alumnos/editar-alumnos.blade.php
+++ resources/views/Alumnos/editar-alumnos.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <h1>Editar Alumno</h1>
     <x-encabezado >
         Edita los datos del alumno
@@ -49,5 +49,6 @@
 
         <button type="submit">Guardar Alumno</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
